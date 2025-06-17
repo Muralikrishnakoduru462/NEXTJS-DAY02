64e9416c194813d384c2791c7bf18cc5caa23163
MuraliKrishna Koduru <muralikrishna.koduru@advance-auto.com>
2025-06-17 16:03:49 +0530

    Next js day02

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+export default function DashboardLayout({children}:{
+children:React.ReactNode;
+}){
+    return (
+        <>
+        {children}
+        <h2>featuredProducts</h2>
+        </>
+    )
+
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+function Dashboard (){
+    return <h1>Dashboard</h1>
+}
+
+export default function Dashboard2(){
+    return <h1>Dashboard2</h1>
+}
